dc5140fb80bb0be93c8fa33455bb9b7aa80fc413
Marina Martin <marinatheballerina@gmail.com>
2019-02-16 20:00:33 -0800

    crystal tests

---
+++ GameJam2019/Assets/Scripts/Crystal1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal1 : MonoBehaviour
+{
+    public int crystalcount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("starttest");
+    }
+
+    void OnTriggerEnter2D(Collider2D playerCollider)
+    {
+        print("player entered collider");
+        //if (playerCollider.tag == "Gem")
+        //{
+        // destroys crystal
+        Destroy(gameObject);
+        // increments count for number of crystals consumes 
+        crystalcount = crystalcount+1;
+        // retuns2
+        print("Crystal Count =" + crystalcount);
+
+        //}
+        //else
+        //{
+        //    print("object wasnt destroyed");
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

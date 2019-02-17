c3ea3fb49dc04aff275b821328b3c1a4b3a1f385
Alex Brown <alex.98.brown@gmail.com>
2019-02-16 18:45:13 -0800

    This stupid interact text now shows up even though some idiot put space as the jump key which is super incorrect

--- GameJam2019/Assets/LevelDoorAction.cs
+++ GameJam2019/Assets/LevelDoorAction.cs
@@ -14,14 +14,14 @@ public class LevelDoorAction : MonoBehaviour {
         BlockPlayer = GetComponent<BoxCollider2D>();
         player = GameObject.Find("player");
         check = true;
-        canv = GameObject.Find("InteractionButtonCanvas");
+        canv = GameObject.FindGameObjectWithTag("interactUI");
     }
 
     // Update is called once per frame
     void Update() {
         if (check == true) {//CheckUnlocked() == True) {
             if (PlayerProximityCollider.IsTouchingLayers(LayerMask.GetMask("Player")) == true) {
-                canv.SetActive(true);
+                canv.transform.GetChild(0).gameObject.SetActive(true);
                 print("yeet");
                 //BlockPlayer.Enabled = False;
                 //Door open animation
@@ -29,7 +29,9 @@ public class LevelDoorAction : MonoBehaviour {
                 //while (player.getComponent<CapsuleCollider)
                  //   player.getComponent<RigidBody2D>.velocity += new Vector2(3, 0);
             }
-            canv.SetActive(false);
+            else {
+                canv.transform.GetChild(0).gameObject.SetActive(false);
+            }
         }
     }
     

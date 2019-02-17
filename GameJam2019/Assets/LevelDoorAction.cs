8f78a02a130f681b7ca6935d7c17c70064973124
Alex Brown <alex.98.brown@gmail.com>
2019-02-16 18:20:07 -0800

    Door Script Started, Defs not finished
    This is so dumb i hate this program why does it have to be so dumbly complicated like what the heck

---
+++ GameJam2019/Assets/LevelDoorAction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDoorAction : MonoBehaviour {
+    CapsuleCollider2D PlayerProximityCollider;
+    BoxCollider2D BlockPlayer;
+    GameObject player;
+    bool check;
+    public GameObject canv;
+    // Start is called before the first frame update
+    void Start() {
+        PlayerProximityCollider = GetComponent<CapsuleCollider2D>();
+        BlockPlayer = GetComponent<BoxCollider2D>();
+        player = GameObject.Find("player");
+        check = true;
+        canv = GameObject.Find("InteractionButtonCanvas");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (check == true) {//CheckUnlocked() == True) {
+            if (PlayerProximityCollider.IsTouchingLayers(LayerMask.GetMask("Player")) == true) {
+                canv.SetActive(true);
+                print("yeet");
+                //BlockPlayer.Enabled = False;
+                //Door open animation
+                //Disable Player Controls
+                //while (player.getComponent<CapsuleCollider)
+                 //   player.getComponent<RigidBody2D>.velocity += new Vector2(3, 0);
+            }
+            canv.SetActive(false);
+        }
+    }
+    
+    /*bool CheckUnlocked() {
+        if (player.GetKey == True && player.AtMinEnergy == True) {
+            return True;
+        }
+        return False;
+    }*/
+}
